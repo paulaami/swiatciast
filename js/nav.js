d3f1784e2384470e555b533c2584f17ec7b785f4
paulaami <pmilkowska@onet.pl>
2021-11-06 16:00:09 +0100

    updated

--- js/nav.js
+++ js/nav.js
@@ -1,21 +1,40 @@
 const burger = document.querySelector('.nav__container-menu');
 const navItem = document.querySelector('.mobile-item');
+const navMobile = document.querySelector('.nav__container-menu-mobile');
+const fixedItem = document.querySelectorAll('.fixed-item');
 
 const showNav = () => {
-    const navMobile = document.querySelector('.nav__container-menu-mobile');
-    const fixedItem = document.querySelectorAll('.fixed-item');
-
+    
     navMobile.classList.toggle('show-nav');
+    fixedItem.forEach(item => item.classList.toggle('show-fixed'));
     document.body.classList.toggle('lock-body-overflow');
-    fixedItem.forEach( item => item.classList.toggle('show-fixed'));
+    const mobileItems = document.querySelectorAll('.nav__link-mobile');
+    
+    mobileItems.forEach( item =>  {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('show-nav');
+            fixedItem.forEach(item => item.classList.remove('show-fixed'));
+        })
+    })
 }
 
+
+
 const showItem = () => {
-    
+
     const listItem = document.querySelector('.dropdown__menu');
 
     listItem.classList.toggle('show');
 }
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    /*Active link*/
+    navLink.forEach(n => n.classList.remove('active-color'));
+    this.classList.add('active-color');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 burger.addEventListener('click', showNav);
 navItem.addEventListener('click', showItem);
